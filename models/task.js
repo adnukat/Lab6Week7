02a952d7a9553a83647eb58e4be9f1f3036a9155
adnukat <taku@dyn-41-188-234-18.its.monash.edu>
2019-09-19 12:49:55 +0200

    intial commit

---
+++ models/task.js
@@ -0,0 +1,14 @@
+//Task Schema
+const mongoose = require('mongoose');
+let taskSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    assignto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Developer'
+    },
+    due: Date,
+    status: String,
+    description: String
+});
+module.exports = mongoose.model('Task', taskSchema);
